53d9181f574fe8e001c0026ec5e9d6796d5e2396
Andrew Marino <drewmarino25@gmail.com>
2023-01-25 10:10:26 -0500

    fixed bugs relating to the collectibles crashing the game

--- Scripts/Collectible.gd
+++ Scripts/Collectible.gd
@@ -9,11 +9,11 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position = Vector2(rand_range(0,1920),rand_range(0,1080));
-	get_parent().move_child(self, 1);
+	#get_parent().move_child(self, 1);
 func _body_entered(body):
 	if (body is KinematicBody2D):
 		body.score+=1;
-	get_parent().remove_child(self);
+	self.queue_free();
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- Scripts/QuitButton.gd
+++ Scripts/QuitButton.gd
@@ -10,7 +10,7 @@ extends Button
 func _ready():
 	pass # Replace with function body.
 func _pressed():
-	get_tree().quit();
+	return get_tree().quit();
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
